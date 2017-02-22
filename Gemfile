c1fe78ae4e81c5871895c482b787cc8ee0713b1f
kenta-s <knt01222@gmail.com>
2017-02-22 22:00:08 +0900

    Add validation tests for Todo

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ end
 
 group :test do
   gem 'rails-controller-testing'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 group :development do
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:status) }
+  it { is_expected.to validate_length_of(:title).is_at_most(100) }
 end
